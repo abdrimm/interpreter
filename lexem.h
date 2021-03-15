d747a4e2f9c05d067e7a8b7fd035265ceee8a4f0
abdrimm <72120477+abdrimm@users.noreply.github.com>
2021-03-15 10:12:00 +0600

    Update lexem.h

--- lexem.h
+++ lexem.h
@@ -3,15 +3,62 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <map>
 using namespace std;
 class Lexem;
 class Number;
 class Operators;
+class Variable;
+
+map <string, Variable*> varTable;
+
 
 enum OPERATOR {
-    PLUS , MINUS ,
-    MULTIPLY ,
-    LBRACKET , RBRACKET
+    LBRACKET, RBRACKET,
+    ASSIGN,
+    OR,
+    AND,
+    BITOR,
+    XOR,
+    BITAND,
+    EQ, NEQ,
+    LEQ, LT,
+    GEQ, GT,
+    SHL, SHR,
+    PLUS, MINUS,
+    MULTIPLY, DIV, MOD
+};
+
+string OPERTEXT[] = {
+    "(", ")",
+    "=",
+    "or",
+    "and",
+    "|",
+    "^",
+    "&",
+    "==", "!=",
+    "<=", "<",
+    ">=", ">",
+    "<<", ">>",
+    "+", "-",
+    "*", "/", "%"
+};
+
+int PRIORITY [] = {
+    -1 , -1 ,
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6, 6,
+    7, 7,
+    7, 7,
+    8, 8,
+    9, 9,
+    10, 10, 10
 };
 class Lexem {
     public :
@@ -19,83 +66,147 @@ class Lexem {
         virtual OPERATOR getType() {};
         virtual bool isOperator() {return false;};
         virtual int getValue() const {};
-        virtual int getValue(const Number &left, const Number &right) {};
+        virtual int getValue(Lexem *left, Lexem *right) {};
         virtual int getPriority() {};
+        virtual void setValue(int value) {};
+        virtual void print() {}; 
 };
+Lexem::Lexem() {
+    
+}
 class Number : public Lexem {
     int value;
     public :
         Number();
         Number(int value);
-        virtual int getValue() const ;
+        virtual bool isOperator() {return false;};
+        virtual int getValue() const;
+        virtual void print();
 };
-class Operators : public Lexem {
-    OPERATOR opertype;
-    public :
-        Operators();
-        Operators(char ch);
-        virtual bool isOperator() {return true;};
-        Operators(vector <Lexem *> vec);
-        virtual OPERATOR getType();
-        virtual int getPriority();
-        virtual int getValue(const Number &left, const Number &right);
-};
-
-int PRIORITY [] = {
-     0 , 0 ,
-     1 , 
-     2 , 2
-};
-char arrayOfOperators[] = {
-    '+', '-',
-    '*',
-    '(', ')'
-};
-char arrayOfBrackets[] = {'(', ')'};
-Number::Number() {
-    value = 0;
-}
-Lexem::Lexem() {
-    
-}
 Number::Number(int value) {
     this->value = value;
 }
 int Number::getValue() const {
     return value;
 }
-Operators::Operators() {}
+void Number::print() {
+    cout << value;
+}
 
+
+class Operators : public Lexem {
+    OPERATOR opertype;
+    public :
+        Operators();
+        Operators(int idx);
+        virtual bool isOperator() {return true;};
+        Operators(vector <Lexem *> vec);
+        virtual OPERATOR getType();
+        virtual int getPriority();
+        virtual int getValue(Lexem *left, Lexem *right);
+        virtual void print();
+};
+Operators::Operators() {}
 OPERATOR Operators::getType() {
     return opertype;
 }
-Operators::Operators(char ch) {
-    int operatorNumber = 0;
-    for(int i = 0; i < sizeof(arrayOfOperators); ++i) {
-        if(ch == arrayOfOperators[i]) {
-            opertype = (OPERATOR)i;    
-        }
-    }  
+Operators::Operators(int idx) {
+    opertype = (OPERATOR)idx;
+}
+void Operators::print() {
+    cout << OPERTEXT[(int)getType()];
 }
 int Operators::getPriority() {
     int priority = 0;
-    for(int i = 0; i < sizeof(arrayOfOperators); ++i) {
+    for(int i = 0; i < sizeof(OPERTEXT); ++i) {
         if(getType() == i) {
             priority = PRIORITY[i]; 
         }
     }
     return priority;
 }
-int Operators::getValue(const Number &left, const Number &right) {  
-    if(getType() == PLUS) {
-        return left.getValue() + right.getValue();
+int Operators::getValue(Lexem *left, Lexem *right) {  
+    if(opertype == PLUS) {
+        return left->getValue() + right->getValue();
     }
-    if(getType() == MINUS) {
-        return left.getValue() - right.getValue();
+    if(opertype == MINUS) {
+        return left->getValue() - right->getValue();
     }
-    if(getType() == MULTIPLY) {
-        return left.getValue() * right.getValue();
+    if(opertype == MULTIPLY) {
+        return left->getValue() * right->getValue();
     }
+    if(opertype == DIV) {
+        return left->getValue() / right->getValue();
+    }
+    if(opertype == MOD) {
+        return left->getValue() % right->getValue();
+    }
+    if(opertype == AND) {
+        return left->getValue() & right->getValue();
+    }
+    if(opertype == OR) {
+        return left->getValue() || right->getValue();
+    }
+    if(opertype == BITOR) {
+        return left->getValue() | right->getValue();
+    }
+    if(opertype == XOR) {
+        return left->getValue() ^ right->getValue();
+    }
+    if(opertype == BITAND) {
+        return left->getValue() & right->getValue();
+    }
+    if(opertype == EQ) {
+        return left->getValue() == right->getValue();
+    }
+    if(opertype == NEQ) {
+        return left->getValue() != right->getValue();
+    }
+    if(opertype == LEQ) {
+        return left->getValue() <= right->getValue();
+    }
+    if(opertype == LT) {
+        return left->getValue() < right->getValue();
+    }
+    if(opertype == GEQ) {
+       return left->getValue() >= right->getValue();
+    }
+    if(opertype == GT) {
+        return left->getValue() > right->getValue();
+    }
+    if(opertype == ASSIGN) {
+        int val = right->getValue();
+        left->setValue(val);
+        return val;
+    }
+}
+
+class Variable : public Lexem {
+    string name;
+    int value;
+    public:
+        Variable();
+        Variable(string name);
+        virtual bool isOperator() {return false;};
+        virtual void print();
+        virtual int getValue() const;
+        virtual void setValue(int value);
+};
+Variable::Variable() {
+    name = nullptr;
+}
+Variable::Variable(string name) {
+    value = 0;
+    this->name = name;
+}
+void Variable::setValue(int value) {
+    this->value = value;
+}
+int Variable::getValue() const {
+    return value;
+}
+void Variable::print() {
+    cout << name << "[" << value << "]";
 }
 
 #endif
